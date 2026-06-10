e669ab0675201de49f0896ec3b8d6ddb52069631
Ubuntu <ubuntu@ip-172-31-28-61.ec2.internal>
2026-06-10 23:21:52 +0000

    fix: flake8 formatting errors

--- app/models.py
+++ app/models.py
@@ -1,6 +1,7 @@
 from sqlalchemy import Column, Integer, String
 from app.database import Base
 
+
 class Student(Base):
     __tablename__ = "students"
 
--- app/tests/test_students.py
+++ app/tests/test_students.py
@@ -3,6 +3,7 @@ def test_create_student(client):
     assert response.status_code == 200
     assert response.json()["reg_no"] == "2212186"
 
+
 def test_get_students(client):
     response = client.get("/students")
     assert response.status_code == 200
